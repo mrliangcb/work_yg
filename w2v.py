e06d4ed09b6651481e71475fc3db25c60054dbb1
mr.liangcb@gmail.com <85559336123lcb>
2020-08-26 21:37:06 +0800

    123

--- w2v.py
+++ w2v.py
@@ -39,15 +39,30 @@ print(text_all)
 print(label_all)
 
 
+def filter_stopwords(words):
+    '''
+    过滤停用词
+    :param seg_list: 切好词的列表 [word1 ,word2 .......]
+    :return: 过滤后的停用词
+    '''
+    return [word for word in words if word not in stop_words]
+
+
 def participle_from_file(text_list):
     """加载数据文件，对文本进行分词"""
     data_list = []
     for line in text_list:
         # jieba.enable_parallel()
+
         words = jieba.cut(line.strip())
+
         word_list = list(words)
+
+        # words = filter_stopwords(words)
+
         # jieba.disable_parallel()
         data_list.append(' '.join(word_list) .strip()) 
+        
     return data_list
 
 fenci_text=participle_from_file(text_all)
@@ -56,7 +71,7 @@ fenci_label=participle_from_file(label_all)
 print(fenci_text)
 print(fenci_label)
 
-
+(?# stop_w_path=r'./stopwords/哈工大停用词表.txt')
 
 
 
